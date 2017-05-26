2f6e059c2de91c5145037bdbbb1c6e86ffa9ea62
ipoemi <ipoemi@nate.com>
2017-05-26 09:23:27 +0900

    Chaper14 Solved

--- exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -2,14 +2,19 @@ package fpinscala.localeffects
 
 import fpinscala.monads._
 
+import scala.collection.mutable.HashMap
+import scala.language.postfixOps
+
 object Mutable {
   def quicksort(xs: List[Int]): List[Int] = if (xs.isEmpty) xs else {
     val arr = xs.toArray
+
     def swap(x: Int, y: Int) = {
       val tmp = arr(x)
       arr(x) = arr(y)
       arr(y) = tmp
     }
+
     def partition(l: Int, r: Int, pivot: Int) = {
       val pivotVal = arr(pivot)
       swap(pivot, r)
@@ -21,25 +26,30 @@ object Mutable {
       swap(j, r)
       j
     }
+
     def qs(l: Int, r: Int): Unit = if (l < r) {
       val pi = partition(l, r, l + (r - l) / 2)
       qs(l, pi - 1)
       qs(pi + 1, r)
     }
+
     qs(0, arr.length - 1)
     arr.toList
   }
 }
 
-sealed trait ST[S,A] { self =>
-  protected def run(s: S): (A,S)
-  def map[B](f: A => B): ST[S,B] = new ST[S,B] {
+sealed trait ST[S, A] {
+  self =>
+  protected def run(s: S): (A, S)
+
+  def map[B](f: A => B): ST[S, B] = new ST[S, B] {
     def run(s: S) = {
       val (a, s1) = self.run(s)
       (f(a), s1)
     }
   }
-  def flatMap[B](f: A => ST[S,B]): ST[S,B] = new ST[S,B] {
+
+  def flatMap[B](f: A => ST[S, B]): ST[S, B] = new ST[S, B] {
     def run(s: S) = {
       val (a, s1) = self.run(s)
       f(a).run(s1)
@@ -48,20 +58,23 @@ sealed trait ST[S,A] { self =>
 }
 
 object ST {
-  def apply[S,A](a: => A) = {
+  def apply[S, A](a: => A) = {
     lazy val memo = a
-    new ST[S,A] {
+    new ST[S, A] {
       def run(s: S) = (memo, s)
     }
   }
+
   def runST[A](st: RunnableST[A]): A =
     st[Null].run(null)._1
 }
 
-sealed trait STRef[S,A] {
+sealed trait STRef[S, A] {
   protected var cell: A
-  def read: ST[S,A] = ST(cell)
-  def write(a: => A): ST[S,Unit] = new ST[S,Unit] {
+
+  def read: ST[S, A] = ST(cell)
+
+  def write(a: => A): ST[S, Unit] = new ST[S, Unit] {
     def run(s: S) = {
       cell = a
       ((), s)
@@ -70,22 +83,23 @@ sealed trait STRef[S,A] {
 }
 
 object STRef {
-  def apply[S,A](a: A): ST[S, STRef[S,A]] = ST(new STRef[S,A] {
+  def apply[S, A](a: A): ST[S, STRef[S, A]] = ST(new STRef[S, A] {
     var cell = a
   })
 }
 
 trait RunnableST[A] {
-  def apply[S]: ST[S,A]
+  def apply[S]: ST[S, A]
 }
 
 // Scala requires an implicit Manifest for constructing arrays.
-sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
+sealed abstract class STArray[S, A](implicit manifest: Manifest[A]) {
   protected def value: Array[A]
-  def size: ST[S,Int] = ST(value.size)
+
+  def size: ST[S, Int] = ST(value.size)
 
   // Write a value at the give index of the array
-  def write(i: Int, a: A): ST[S,Unit] = new ST[S,Unit] {
+  def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
     def run(s: S) = {
       value(i) = a
       ((), s)
@@ -93,14 +107,20 @@ sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
   }
 
   // Read the value at the given index of the array
-  def read(i: Int): ST[S,A] = ST(value(i))
+  def read(i: Int): ST[S, A] = ST(value(i))
 
   // Turn the array into an immutable list
-  def freeze: ST[S,List[A]] = ST(value.toList)
+  def freeze: ST[S, List[A]] = ST(value.toList)
 
-  def fill(xs: Map[Int,A]): ST[S,Unit] = ???
+  def fill(xs: Map[Int, A]): ST[S, Unit] =
+  //xs.keys.foldLeft(ST[S, Unit](())) { (acc, k) =>
+  //  acc flatMap { _ =>
+  //    write(k, xs(k))
+  //  }
+  //}
+    xs.foldRight(ST[S, Unit](())) { case ((k, v), st) => st flatMap (_ => write(k, v)) }
 
-  def swap(i: Int, j: Int): ST[S,Unit] = for {
+  def swap(i: Int, j: Int): ST[S, Unit] = for {
     x <- read(i)
     y <- read(j)
     _ <- write(i, y)
@@ -110,34 +130,122 @@ sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
 
 object STArray {
   // Construct an array of the given size filled with the value v
-  def apply[S,A:Manifest](sz: Int, v: A): ST[S, STArray[S,A]] =
-    ST(new STArray[S,A] {
+  def apply[S, A: Manifest](sz: Int, v: A): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A] {
       lazy val value = Array.fill(sz)(v)
     })
 
-  def fromList[S,A:Manifest](xs: List[A]): ST[S, STArray[S,A]] =
-    ST(new STArray[S,A] {
+  def fromList[S, A: Manifest](xs: List[A]): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A] {
       lazy val value = xs.toArray
     })
 }
 
+sealed trait STMap[S, K, V] {
+  protected def table: HashMap[K, V]
+
+  def size: ST[S, Int] = ST(table.size)
+
+  // Get the value under a key
+  def apply(k: K): ST[S, V] = ST(table(k))
+
+  // Get the value under a key, or None if the key does not exist
+  def get(k: K): ST[S, Option[V]] = ST(table.get(k))
+
+  // Add a value under a key
+  def +=(kv: (K, V)): ST[S, Unit] = ST(table += kv)
+
+  // Remove a key
+  def -=(k: K): ST[S, Unit] = ST(table -= k)
+}
+
+object STMap {
+  def empty[S, K, V]: ST[S, STMap[S, K, V]] = ST(new STMap[S, K, V] {
+    val table = HashMap.empty[K, V]
+  })
+
+  def fromMap[S, K, V](m: Map[K, V]): ST[S, STMap[S, K, V]] = ST(new STMap[S, K, V] {
+    val table = (HashMap.newBuilder[K, V] ++= m).result
+  })
+}
+
 object Immutable {
-  def noop[S] = ST[S,Unit](())
+  def noop[S] = ST[S, Unit](())
 
-  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = ???
+  def foreach[S, A](l: Stream[A])(f: A => ST[S, Unit]): ST[S, Unit] = l match {
+    case h #:: t => f(h) flatMap (_ => foreach(t)(f))
+    case _ => noop
+  }
 
-  def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] = ???
+  /*
+  def partition[S](arr: STArray[S, Int], l: Int, r: Int, pivot: Int): ST[S, Int] = for {
+    pivotVal <- arr.read(pivot)
+    _ <- arr.swap(pivot, r)
+    jr <- STRef[S, Int](l)
+    _ <- foreach[S, Int](l until r toStream)(i => for {
+      iv <- arr.read(i)
+      _ <- if (iv < pivotVal) for {
+        j <- jr.read
+        _ <- arr.swap(i, j)
+        _ <- jr.write(j + 1)
+      } yield () else noop[S]
+    } yield ())
+    j <- jr.read
+    _ <- arr.swap(j, r)
+  } yield j
+  */
+  def partition[S](arr: STArray[S, Int], l: Int, r: Int, pivot: Int): ST[S, Int] = for {
+    pivotVal <- arr.read(pivot)
+    _ <- arr.swap(pivot, r)
+    jr <- STRef[S, Int](l)
+    _ <- (l until r).foldLeft(noop[S])((s, i) => for {
+      iv <- arr.read(i)
+      _ <- if (iv < pivotVal) for {
+        j <- jr.read
+        _ <- arr.swap(i, j)
+        _ <- jr.write(j + 1)
+      } yield () else noop[S]
+    } yield ())
+    j <- jr.read
+    _ <- arr.swap(j, r)
+  } yield j
+
+  def qs[S](a: STArray[S, Int], l: Int, r: Int): ST[S, Unit] = if (l < r) for {
+    pi <- partition(a, l, r, l + (r - l) / 2)
+    _ <- qs(a, l, pi - 1)
+    _ <- qs(a, pi + 1, r)
+  } yield () else noop[S]
 
   def quicksort(xs: List[Int]): List[Int] =
     if (xs.isEmpty) xs else ST.runST(new RunnableST[List[Int]] {
       def apply[S] = for {
-        arr    <- STArray.fromList(xs)
-        size   <- arr.size
-        _      <- qs(arr, 0, size - 1)
+        arr <- STArray.fromList(xs)
+        size <- arr.size
+        _ <- qs(arr, 0, size - 1)
         sorted <- arr.freeze
       } yield sorted
-  })
+    })
 }
 
 import scala.collection.mutable.HashMap
 
+object Main {
+  def main(args: Array[String]): Unit = {
+    def p[T] = new RunnableST[STRef[_, Int]] {
+      def apply[S] = for {
+        r1 <- STRef(1)
+      } yield r1
+    }
+
+    val stref = ST.runST(p)
+
+    //val p2 = new RunnableST[STRef[_, Int]] {
+    //  def apply[S] = for {
+    //    r1 <- stref
+    //  } yield r1
+    //}
+    //val stref2 = ST.runST(p2)
+
+    //println(Immutable.quicksort(List(5, 2, 3)))
+  }
+}
